2248da3b0e4901eb2394e62280a18c806db4ba69
Jerry Lee <oldratlee@gmail.com>
2014-06-12 22:51:54 +0800

    reformat code

--- src/main/java/examples/plotter/Function.java
+++ src/main/java/examples/plotter/Function.java
@@ -9,13 +9,12 @@ package examples.plotter;
  * that maps double to doubles.
  */
 public interface Function {
-   /**
-    * Compute a value {@code y=f(x)} of an dependent variable y from an
-    * independent variable x
-    * 
-    * @param x
-    *           the input value
-    * @return the result of a mathematical function f(x)
-    */
-   double f(double x);
+    /**
+     * Compute a value {@code y=f(x)} of an dependent variable y from an
+     * independent variable x
+     *
+     * @param x the input value
+     * @return the result of a mathematical function f(x)
+     */
+    double f(double x);
 }
--- src/main/java/examples/plotter/Plotter.java
+++ src/main/java/examples/plotter/Plotter.java
@@ -4,52 +4,50 @@
 
 package examples.plotter;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 /**
  * This is a Swing application which demonstrates the use of
  * javax.tools.Compiler via the Facade class,
  * javaxtools.compiler.CharSequenceCompiler. See {@link PlotterPanel} for more
  * information.
- * 
+ *
  * @author <a href="mailto:David.Biesack@sas.com">David J. Biesack</a>
- * 
  */
 final public class Plotter extends JFrame {
 
-   private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-   /**
-    * Display the plot GUI and allow user interaction.
-    * 
-    * @param args
-    */
-   public static void main(final String[] args) {
-      try {
-         new Plotter().setVisible(true);
-      } catch (Throwable e) {
-         presentException(e);
-      }
-   }
+    /**
+     * Display the plot GUI and allow user interaction.
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        try {
+            new Plotter().setVisible(true);
+        } catch (Throwable e) {
+            presentException(e);
+        }
+    }
 
-   static void presentException(Throwable t) {
-      String title = "Unable to run the " + Plotter.class.getName() + " application.";
-      String message = title
-            + " \n"
-            + "This may be due to a missing tools.jar or missing JFreeChart jars. \n"
-            + "Please consult the docs/README file found with this application for further details.";
-      JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
-   }
+    static void presentException(Throwable t) {
+        String title = "Unable to run the " + Plotter.class.getName() + " application.";
+        String message = title
+                + " \n"
+                + "This may be due to a missing tools.jar or missing JFreeChart jars. \n"
+                + "Please consult the docs/README file found with this application for further details.";
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
+    }
 
-   /**
-    * Default constructor for the plotter application
-    */
-   public Plotter() {
-      super("javaxtools.compiler demo");
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      getContentPane().add(new PlotterPanel());
-      pack();
-   }
+    /**
+     * Default constructor for the plotter application
+     */
+    public Plotter() {
+        super("javaxtools.compiler demo");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().add(new PlotterPanel());
+        pack();
+    }
 
 }
